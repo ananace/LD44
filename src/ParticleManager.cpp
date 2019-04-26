a72d5572677bb50658dc67fbdc172310fe5a65f8
Alexander "Ace" Olofsson <ace@haxalot.com>
2019-04-26 23:52:57 +0200

    Fix particle drawing

--- src/ParticleManager.cpp
+++ src/ParticleManager.cpp
@@ -7,27 +7,27 @@
 #include <algorithm>
 #include <cmath>
 
-inline void _setupQuad(const ParticleManager::Particle& particle, sf::Vertex quad[5], sf::Vector2f& axises, sf::Vector2f& size)
+inline void _setupQuad(const ParticleManager::Particle& particle, sf::Vertex quad[5], sf::Vector2f axises[2], sf::Vector2f& size)
 {
-    axises.x = std::sin(particle.Angle);
-    axises.y = std::cos(particle.Angle);
+    axises[0] = sf::Vector2f(std::cos(particle.Angle), std::sin(particle.Angle));
+    axises[1] = sf::Vector2f(std::cos(particle.Angle + Pi()/2.f), std::sin(particle.Angle + Pi()/2.f));
     size.x = particle.TexCoords.width;
     size.y = particle.TexCoords.height;
 
     quad[0].color = particle.Vertex.color;
-    quad[0].position = particle.Vertex.position + axises * sf::Vector2f(-0.5, -0.5) * size;
+    quad[0].position = particle.Vertex.position + (size.x * -0.5f * axises[0]) + (size.y * -0.5f * axises[1]);
     quad[0].texCoords = sf::Vector2f(particle.TexCoords.left, particle.TexCoords.top);
     quad[1].color = particle.Vertex.color;
-    quad[1].position = particle.Vertex.position + axises * sf::Vector2f(0.5, -0.5) * size;
+    quad[1].position = particle.Vertex.position + (size.x * 0.5f * axises[0]) + (size.y * -0.5f * axises[1]);
     quad[1].texCoords = sf::Vector2f(particle.TexCoords.left + particle.TexCoords.width, particle.TexCoords.top);
     quad[2].color = particle.Vertex.color;
-    quad[2].position = particle.Vertex.position + axises * sf::Vector2f(0.5, 0.5) * size;
+    quad[2].position = particle.Vertex.position + (size.x * 0.5f * axises[0]) + (size.y * 0.5f * axises[1]);
     quad[2].texCoords = sf::Vector2f(particle.TexCoords.left + particle.TexCoords.width, particle.TexCoords.top + particle.TexCoords.height);
     quad[3].color = particle.Vertex.color;
-    quad[3].position = particle.Vertex.position + axises * sf::Vector2f(-0.5, 0.5) * size;
+    quad[3].position = particle.Vertex.position + (size.x * -0.5f * axises[0]) + (size.y * 0.5f * axises[1]);
     quad[3].texCoords = sf::Vector2f(particle.TexCoords.left, particle.TexCoords.top + particle.TexCoords.height);
     quad[4].color = particle.Vertex.color;
-    quad[4].position = particle.Vertex.position + axises * sf::Vector2f(-0.5, -0.5) * size;
+    quad[4].position = particle.Vertex.position + (size.x * -0.5f * axises[0]) + (size.y * -0.5f * axises[1]);
     quad[4].texCoords = sf::Vector2f(particle.TexCoords.left, particle.TexCoords.top);
 }
 
@@ -58,10 +58,11 @@ void ParticleManager::update(float aDt)
 
 void ParticleManager::drawAbove(sf::RenderTarget& aTarget)
 {
-    // sf::RenderStates states(aTarget.getView().getTransform());
-    // states.texture = &m_particleSheet;
+    sf::RenderStates states;
+    states.texture = &m_particleSheet;
 
-    sf::Vector2f axises, size;
+    sf::Vector2f axises[2];
+    sf::Vector2f size;
     sf::Vertex quad[5];
 
     for (auto& particle : m_particles)
@@ -70,15 +71,16 @@ void ParticleManager::drawAbove(sf::RenderTarget& aTarget)
             continue;
 
         _setupQuad(particle, quad, axises, size);
-        aTarget.draw(quad, 5, sf::TriangleStrip);
+        aTarget.draw(quad, 5, sf::TriangleStrip, states);
     }
 }
 void ParticleManager::drawBelow(sf::RenderTarget& aTarget)
 {
-    // sf::RenderStates states(aTarget.getView().getTransform());
-    // states.texture = &m_particleSheet;
+    sf::RenderStates states;
+    states.texture = &m_particleSheet;
 
-    sf::Vector2f axises, size;
+    sf::Vector2f axises[2];
+    sf::Vector2f size;
     sf::Vertex quad[5];
 
     for (auto& particle : m_particles)
@@ -87,6 +89,6 @@ void ParticleManager::drawBelow(sf::RenderTarget& aTarget)
             continue;
 
         _setupQuad(particle, quad, axises, size);
-        aTarget.draw(quad, 5, sf::TriangleStrip);
+        aTarget.draw(quad, 5, sf::TriangleStrip, states);
     }
 }
